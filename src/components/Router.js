3a1284e5d58e5ea4867f86d1384cc2149f9c535c
joamksh <joamksh@naver.com>
2024-01-31 12:00:24 +0900

    7-1 complete

--- src/components/Router.js
+++ src/components/Router.js
@@ -3,15 +3,17 @@ import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Auth from "routes/Auth";
 import Home from "routes/Home";
 import Profile from "routes/Profile";
+import Navigation from "./Navigation";
 
 const AppRouter = ({ isLoggedIn, userObj }) => {
   return (
     <Router>
+      {isLoggedIn &&<Navigation/>}
       <Routes>
         {isLoggedIn ? (
           <>
             <Route path="/" element={<Home userObj={userObj} />} />
-            <Route path="/profile" element={<Profile />} />
+            <Route path="/profile" element={<Profile userObj={userObj} />} />
           </>
         ) : (
           <Route path="/" element={<Auth />} />
--- src/routes/Profile.js
+++ src/routes/Profile.js
@@ -1,8 +1,9 @@
-import React from "react";
-import { authService } from "fbase";
-import { useNavigate } from "react-router-dom"; // useNavigate를 import
+// Profile.js
+import React, { useEffect } from "react";
+import { authService, dbService } from "../fbase";
+import { useNavigate } from "react-router-dom";
 
-const Profile = () => {
+const Profile = ({ userObj }) => {
     const navigate = useNavigate();
 
     const onLogoutClick = () => {
@@ -10,9 +11,28 @@ const Profile = () => {
         navigate("/");
     };
 
+    const getMyNweets = async () => {
+        try {
+            const nweets = await dbService
+                .collection("nweets")
+                .where("creatorId", "==", userObj.uid)
+                .orderBy("createdAt", "asc")
+                .get();
+
+            console.log(nweets.docs.map((doc) => doc.data()));
+        } catch (error) {
+            //console.error("에러 발생:", error);
+        }
+    };
+
+    useEffect(() => {
+        getMyNweets();
+    }, []);
+
     return (
         <div>
             <h2>Profile</h2>
+            <p>user ID: {userObj.uid}</p>
             <button onClick={onLogoutClick}>Log Out</button>
         </div>
     );
